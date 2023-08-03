86aa2dd1f6e85ad251d1a0184aaedf92c2241529
Isabele Leal <isabeleleal55@gmail.com>
2023-08-03 09:25:46 -0300

    Adicionando código pingpong

---
+++ Cat_debt_2023_08_03_12_23_39/sketch.js
@@ -0,0 +1,106 @@
+// mostra Bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+
+let raio = diametro/2;
+
+//velocidade bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha =6;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+// mostra minha Raquete
+let xRaquete = 5;
+let yRaquete = 150;
+
+// mostra raquete oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+
+// movimento raquete oponente
+let velocidadeYRaqueteOponente = 6;
+
+
+let colidiu = false;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentoBolinha();
+  colisaoBolinha();
+  mostraMinhaRaquete(xRaquete, yRaquete);
+  movimentoMinhaRaquete();
+  //colisaoMinhaRaquete(xRaquete, yRaquete);
+  
+  mostraMinhaRaquete(xRaqueteOponente, yRaqueteOponente);
+  movimentoRaqueteOponente();
+  colisaoMinhaRaqueteBiblioteca(xRaquete, yRaquete);
+  colisaoMinhaRaqueteBiblioteca(xRaqueteOponente, yRaqueteOponente);
+  
+}
+
+function mostraBolinha(){
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentoBolinha(){
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function colisaoBolinha(){
+  if(xBolinha + raio > width || xBolinha - raio < 0){
+    velocidadeXBolinha *= -1;
+  }
+  if(yBolinha + raio > height || yBolinha -raio < 0){
+    velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraMinhaRaquete(x,y){
+  rect(x, y, raqueteComprimento, raqueteAltura);
+  
+}
+function colisaoMinhaRaqueteBiblioteca(x, y) {
+    colidiu = collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+    if (colidiu) {
+        velocidadeXBolinha *= -1;
+    }
+}
+
+
+function movimentoMinhaRaquete(){
+   if (keyIsDown(UP_ARROW)) {
+        yRaquete -= 10;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        yRaquete += 10;
+    }
+   // Vamos limitar a movimentação da raquete para que ela não ultrapasse as bordas:
+    yRaquete = constrain(yRaquete, 10, 310);
+}
+
+function colisaoMinhaRaquete(x,y){
+   if(xBolinha - raio < x + raqueteComprimento && yBolinha - raio < y + raqueteAltura && yBolinha - raio > y ){
+     velocidadeXBolinha *= -1;
+   }
+
+  
+}
+
+
+function movimentoRaqueteOponente(){
+  velocidadeYRaqueteOponente = yBolinha - yRaqueteOponente - raqueteComprimento / 2 - 30;
+  yRaqueteOponente += velocidadeYRaqueteOponente;
+  
+  
+    // Vamos limitar a movimentação da raquete para que ela não ultrapasse as bordas:
+    yRaqueteOponente = constrain(yRaqueteOponente, 10, 310);
+}
